9d4f8a6137798636a6ae5e0c369e085f5ed6fa8f
WinzeTim <154031667+WinzeTim@users.noreply.github.com>
2025-05-03 10:27:37 -0400

    Update ChannelSetting.js

--- ChannelSetting.js
+++ ChannelSetting.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 const channelSettingSchema = new mongoose.Schema({
   guildId: String,
-  channelId: String, // default stock channel
-  weatherChannelId: String, // weather-specific channel
-  roles: { type: Object, default: {} }
+  channelId: String,         // default stock channel
+  weatherChannelId: String,  // weather-specific channel
+  petChannelId: String,      // pet/egg-specific channel
+  roles: { type: Object, default: {} } // roles to ping by keyword
 });
 
 module.exports = mongoose.model('ChannelSetting', channelSettingSchema);
