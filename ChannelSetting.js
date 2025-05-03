ab2d5995f6d98b3326295de4da41e8c8e226cbe4
WinzeTim <154031667+WinzeTim@users.noreply.github.com>
2025-05-03 08:53:03 -0400

    Update ChannelSetting.js

--- ChannelSetting.js
+++ ChannelSetting.js
@@ -1,10 +1,10 @@
-// ChannelSetting.js
 const mongoose = require('mongoose');
 
 const channelSettingSchema = new mongoose.Schema({
-  guildId: { type: String, required: true },
-  channelId: { type: String, required: true },
-  roles: [String]
+  guildId: String,
+  channelId: String, // default stock channel
+  weatherChannelId: String, // weather-specific channel
+  roles: { type: Object, default: {} }
 });
 
 module.exports = mongoose.model('ChannelSetting', channelSettingSchema);
